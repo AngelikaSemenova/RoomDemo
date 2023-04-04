e4accc4476ec771387b664a6cc5abdd86c1a2ac1
Anzhelika Kniazeva <anzhelika.kniazeva@network.global>
2023-04-04 14:13:00 +0400

    Create demo app for showing work of Room db

---
+++ app/src/main/java/com/marquiseanges/roomdemo/EmployeeApp.kt
@@ -0,0 +1,11 @@
+package com.marquiseanges.roomdemo
+
+import android.app.Application
+
+class EmployeeApp:Application() {
+
+    val db by lazy {
+        EmployeeDatabase.getInstance(this)
+    }
+
+}
